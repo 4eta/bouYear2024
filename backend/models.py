30b5672bc515519b2d1bd17b9161bc3e7385c4de
4eta <ikizawas@gmail.com>
2024-10-08 14:56:13 +0900

    dbとmodelを作成

---
+++ backend/models.py
@@ -0,0 +1,86 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Float
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.sqltypes import DateTime
+
+from .database import Base
+
+"""
+- DB内につくるテーブルをclassとして定義
+- sqlalchemyのBaseを継承
+"""
+
+class User(Base):
+    """
+    Userテーブル(users)の定義  
+    user_id: ユーザーID  
+    first_name: ユーザーの名  
+    last_name: ユーザーの姓  
+    affiliation: 所属  
+    role: 役職(admin or participant)  
+    """
+    __tablename__ = "users"
+
+    user_id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String(255), not_null=True)
+    last_name = Column(String(255), not_null=True)
+    affiliation = Column(String(255), not_null=True)
+    role = Column(String(255), not_null=True)
+
+
+class Questions(Base):
+    """
+    Questionsテーブル(questions)の定義
+    question_id: 問題ID
+    content: 問題文
+    description: 問題の説明
+    """
+    __tablename__ = "questions"
+
+    question_id = Column(Integer, primary_key=True, index=True)
+    content = Column(String(255), not_null=True)
+    description = Column(String(255), not_null=True)
+
+class Answers(Base):
+    """
+    Answersテーブル(answers)の定義
+    answer_id: 回答ID
+    user_id: ユーザーID
+    question_id: 問題ID
+    content: 回答内容
+    timestamp: 回答日時
+    """
+    __tablename__ = "answers"
+
+    answer_id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.user_id"))
+    question_id = Column(Integer, ForeignKey("questions.question_id"))
+    content = Column(String(255), not_null=True)
+    timestamp = Column(DateTime, not_null=True)
+
+class Scores(Base):
+    """
+    Scoresテーブル(scores)の定義
+    user_id: ユーザーID
+    question_id: 問題ID
+    score: この問題のスコア
+    rank: この問題の順位
+    """
+    __tablename__ = "scores"
+
+    user_id = Column(Integer, ForeignKey("users.user_id"), primary_key=True)
+    question_id = Column(Integer, ForeignKey("questions.question_id"), primary_key=True)
+    score = Column(Float, not_null=True)
+    rank = Column(Integer, not_null=True)
+
+class TotalScores(Base):
+    """
+    TotalScoresテーブル(total_scores)の定義
+    user_id: ユーザーID
+    total_score: ユーザーのこれまでの合計スコア
+    rank: ユーザーの順位
+    """
+    __tablename__ = "total_scores"
+
+    user_id = Column(Integer, ForeignKey("users.user_id"), primary_key=True)
+    total_score = Column(Float, not_null=True)
+    rank = Column(Integer, not_null=True)
